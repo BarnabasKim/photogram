c18be563c5f9c2ab268455812fb09eadabb53f2f
BarnabasKim <dowon8618@gmail.com>
2022-02-10 23:24:37 +0900

    2022-02-10

---
+++ src/main/java/com/cos/photogramstart/handler/ex/CustomValidationException.java
@@ -0,0 +1,24 @@
+package com.cos.photogramstart.handler.ex;
+
+import java.util.Map;
+
+public class CustomValidationException extends RuntimeException{
+
+	//시리얼 번호는 객체를 구분할때!! 
+	private static final long serialVersionUID = 1L;
+	
+	
+	private Map<String, String > errorMap;
+	
+	public CustomValidationException(String message,  Map<String, String > errorMap ) {
+		super(message); // 부모한테 던진다 자신이가지고있는것이 아니라 
+		this.errorMap = errorMap;
+	}
+	
+	//eroorMap을 리턴해주는 getter을 만든다 
+	
+	public Map<String,String> getErrorMap(){
+		return errorMap;
+	}
+
+}
