02e66cfaf7763b8398c99d1873f83e6e60b97fe3
BarnabasKim <dowon8618@gmail.com>
2022-02-11 23:59:32 +0900

    로그인 & View 완료

--- src/main/java/com/cos/photogramstart/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/cos/photogramstart/config/auth/PrincipalDetailsService.java
@@ -5,13 +5,31 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.domain.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service // 어노테이션 붙이는 순간 IOC가 된다. 
 public class PrincipalDetailsService implements UserDetailsService {
 
+	private final UserRepository userRepository;
+	
+	// 1. 패스워드는 알아서 체킹하니까 신경쓸 필요 없다.
+	// 2.리턴이 잘되면 자동으로 UserDetails 타입을 세션으로 만든다.
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		System.out.println("나 실행돼?" + username);
-		return null;
+
+		User userEntity = userRepository.findByUsername(username);
+		
+		if(userEntity == null) {			//null이면 찾지 못했다는것
+			return null;
+		}else {
+			return new PrincipalDetails(userEntity);
+		}
+		
+		
 	}
 
 }
