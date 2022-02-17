6ba5b8aa5ffd0bc265f8cc29ee391d31db7cda36
BarnabasKim <dowon8618@gmail.com>
2022-02-17 13:04:37 +0900

    2022-02-17 Likes View Rendering Completed

--- src/main/resources/static/js/story.js
+++ src/main/resources/static/js/story.js
@@ -44,12 +44,20 @@ function getStoryItem(image) {
 	<div class="sl__item__contents">
 		<div class="sl__item__contents__icon">
 
-			<button>
-				<i class="fas fa-heart active" id="storyLikeIcon-${image.id}" onclick="toggleLike(${image.id})"></i>
+			<button>`;
+
+				if(image.likeState) {
+			item +=`	<i class="fas fa-heart active" id="storyLikeIcon-${image.id}" onclick="toggleLike(${image.id})"></i>`;
+				}else {
+					item +=`	<i class="far fa-heart" id="storyLikeIcon-${image.id}" onclick="toggleLike(${image.id})"></i>`;
+				}
+				
+				
+				item += `
 	</button>
 </div>
 
-<span class="like"><b id="storyLikeCount-1">3 </b>likes</span>
+<span class="like"><b id="storyLikeCount-${image.id}">${image.likeCount} </b>likes</span>
 
 <div class="sl__item__contents__content">
 	<p>${image.caption}</p>
@@ -87,7 +95,7 @@ $(window).scroll(() => {
 	//console.log("윈도우 높이",$(window).height());
 	
 	let checkNum = $(window).scrollTop() - ($(document).height() - $(window).height() );
-	console.log(checkNum);
+	
 	
 	if(checkNum <1 && checkNum > -1){
 			page++;
@@ -99,14 +107,46 @@ $(window).scroll(() => {
 // (3) 좋아요, 안좋아요
 function toggleLike(imageId) {
 	let likeIcon = $(`#storyLikeIcon-${imageId}`);
-	if (likeIcon.hasClass("far")) {
+	
+	if (likeIcon.hasClass("far")) { // 좋아요 하겠다 
+	
+	$.ajax({
+		type:"post",
+		url:`/api/image/${imageId}/likes`,
+		dataType: "json"
+	}).done(res=> {
+		
+		let likeCountStr = $(`#storyLikeCount-${imageId}`).text();
+		let likeCount = Number(likeCountStr) + 1;
+	   $(`#storyLikeCount-${imageId}`).text(likeCount);
+		
 		likeIcon.addClass("fas");
 		likeIcon.addClass("active");
 		likeIcon.removeClass("far");
-	} else {
+	}).fail(error=>{
+		console.log("오류",error);
+	});
+	
+	} else { // 좋아요를 취소하겠다 
+		
+			$.ajax({
+		type:"delete",
+		url:`/api/image/${imageId}/likes`,
+		dataType: "json"
+	}).done(res=> {
+		
+	   let likeCountStr = $(`#storyLikeCount-${imageId}`).text();
+		let likeCount = Number(likeCountStr) - 1;
+	   $(`#storyLikeCount-${imageId}`).text(likeCount);
+		
 		likeIcon.removeClass("fas");
 		likeIcon.removeClass("active");
 		likeIcon.addClass("far");
+	}).fail(error=>{
+		console.log("오류",error);
+	});
+		
+
 	}
 }
 
