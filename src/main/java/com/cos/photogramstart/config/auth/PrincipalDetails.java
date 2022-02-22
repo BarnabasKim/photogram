6809d6ad2db5c817a548b8ce134d6721b6f78306
BarnabasKim <dowon8618@gmail.com>
2022-02-22 11:55:41 +0900

    2022-02-22 Update SNS Project

--- src/main/java/com/cos/photogramstart/config/auth/PrincipalDetails.java
+++ src/main/java/com/cos/photogramstart/config/auth/PrincipalDetails.java
@@ -2,26 +2,32 @@ package com.cos.photogramstart.config.auth;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import com.cos.photogramstart.domain.user.User;
 
 import lombok.Data;
 
 @Data
-public class PrincipalDetails implements UserDetails{
+public class PrincipalDetails implements UserDetails, OAuth2User{
 
 
 	private static final long serialVersionUID = 1L;
 
 	private User user;
+	private Map<String, Object> attributes;
 	
 	public PrincipalDetails(User user) {
 		this.user = user;
 	}
 	
+	public PrincipalDetails(User user, Map<String, Object> attributes ) {
+		this.user = user;
+	}
 	// 권한: 한개가 아닐 수 있음. (3개 이상의 권한) 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -60,6 +66,17 @@ public class PrincipalDetails implements UserDetails{
 	@Override
 	public boolean isEnabled() {
 		return true;
+	}
+
+	@Override
+	public Map<String, Object> getAttributes() {
+		return attributes; // {id: 34314134134134, name:김도원, email:gkrskfo@nate.com}
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return (String) attributes.get("name");
 	}
 
 }
